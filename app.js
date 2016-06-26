bd02f84fe73b9b3aacbd7900703f8f90df105631
Katie Vogel <katiebvogel@gmail.com>
2016-06-25 19:27:02 -0500

    Please enter the commit message for your changes. Lines starting

--- app.js
+++ app.js
@@ -8,7 +8,7 @@ var claim4 = new Claim("Sharon Smith", "Emergency", 1300);
 
 var claim5 = new Claim("Steve Wright", "Primary Care", 770);
 
-var initialList = [claim1, claim2, claim3, claim4, claim5]
+var initialList = [claim1, claim2, claim3, claim4, claim5];
 
 var totalPayedOut = 0;
 
@@ -18,6 +18,91 @@ function Claim(name, type, cost){
 	this.visitCost = cost;
 }
 
+var claim6 = new Claim("Katie Vogel", "Primary Care", 5000);
+
+var claim7 = new Claim("John Arends", "Optical", 250);
+
+var claim8 = new Claim("Nora Schull", "Specialist", 700);
+
+var claim9 = new Claim("Margaret Selva", "Emergency", 1200);
+
+var claim10 = new Claim("Jessica Arends", "Primary Care", 450);
+
+
+
+
+initialList.push(claim6, claim7, claim8, claim9, claim10);
+
+
+
+
+
+
 //function to determine percent covered
-//adding some changes to check my github abilities (which are in doubt)
+
+
+function percentCovered(i){
+	var coverage = 0;
+
+	if(initialList[i].visitType === "Optical"){
+		coverage += 0;
+	}
+	 if (initialList[i].visitType === "Specialist"){
+		 coverage += .1;
+		}
+	if (initialList[i].visitType === "Emergency"){
+		coverage += 1;
+	}
+	if (initialList[i].visitType === "Primary Care"){
+		coverage += .5;
+	}
+
+console.log("The percent covered for claim" + (i + 1) + " is " + coverage * 100 + "%.");
+return coverage;
+}
+
+
+
 //function to determine amount covered
+
+function amountCovered(i){
+	var totalCost = initialList[i].visitCost;
+	var percent = percentCovered(i);
+	var name = initialList[i].patientName;
+	var amount = 0;
+		amount += Math.round(totalCost*percent);
+
+		console.log("The total amount paid for claim" + (i + 1) + " is $" + amount + ".");
+
+		console.log("Paid out $" + amount + " for " + name + ".");
+		return amount;
+	}
+
+//function that will print out entire list of information for all claims
+//and calculates total amount paid to ALL claims in the list
+function allClaims(initialList) {
+var amount = 0;
+	for (i = 0; i < initialList.length; i++){
+		function amountCovered(i){
+			var totalCost = initialList[i].visitCost;
+			var percent = percentCovered(i);
+			var name = initialList[i].patientName;
+			// var amount = 0;
+				amount += Math.round(totalCost*percent);
+
+				console.log("The total amount paid for claim" + (i + 1) + " is $" + amount + ".");
+
+				console.log("Paid out $" + amount + " for " + name + ".");
+				return amount;
+			}
+
+	}
+}
+
+//Here is a for loop that will run through all the claims in the list and
+//find a final total amount paid to all claims
+var totalPayedOut = 0;
+for (i = 0; i < initialList.length; i ++) {
+	totalPayedOut += amountCovered(i);
+}
+	console.log("The total paid for all claims is: $" + totalPayedOut + ".");
